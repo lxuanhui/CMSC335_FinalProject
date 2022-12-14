f9e1c3f10fcb883e81f5a836abc0a966c88285e3
lxuanhui <leex0054@e.ntu.edu.sg>
2022-12-14 18:26:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:false}));
 app.set("view engine", "ejs");
-app.set("views", path.resolve(__dirname, "views"));
+
 require("dotenv").config({ path: path.resolve(__dirname, 'credentials/.env') })  
 const port = process.argv[2] || 3000;
 
@@ -39,39 +39,6 @@ async function main() {
   }
 }
 
-async function insertApplication(client, dbName, collectionName, newApplication) {
-  const result = await client.db(dbName).collection(collectionName).insertOne(newApplication);
-  console.log(`New Application Added ${result.insertedId}`);
-}
-async function lookUpOneEntry(client, dbName,collectionName, email) {
-  let filter = {email: email};
-  const result = await client.db(dbName)
-                      .collection(collectionName)
-                      .findOne(filter);
- if (result) {
-     console.log(result);
-     return result;
- } else {
-     console.log(`No record found with name ${email}`);
- }
-}
-
-async function lookUpMany(client, dbName, collectionName, GPA) {
-  GPA = parseFloat(GPA);
-  let filter = {GPA:{$gte:GPA}};
-  const cursor = client.db(dbName)
-  .collection(collectionName)
-  .find(filter);
-  const result = await cursor.toArray();
-  console.log(result);
-  if (result) {
-      console.log(result);
-      return result;
-  } else {
-      console.log(`No records found with GPA ${GPA}`);
-  }
-};
-main().catch(console.error);
 
 
 
